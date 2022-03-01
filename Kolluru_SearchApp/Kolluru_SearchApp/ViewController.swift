aa459b5cf1ae201f19e712fc1340e41317e3b35b
anuragkolluru <89211878+anuragkolluru@users.noreply.github.com>
2022-03-01 15:09:56 -0600

    SearchApp

---
+++ Kolluru_SearchApp/Kolluru_SearchApp/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Kolluru_SearchApp
+//
+//  Created by Kolluru,Anurag on 3/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var resultImage: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func searchButtonAction(_ sender: Any) {
+    }
+    
+}
+
